84fd6f73f8a2861644384f33c4ea8e03c2019b48
530487 <530487@student.saxion.nl>
2024-12-30 20:17:24 +0100

    Add initial frontend for EasyDevOps app

---
+++ frontend/EasyDevOpsFrontend/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace EasyDevOpsFrontend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to EasyDevOps by ITM!");
+        }
+    }
+}
